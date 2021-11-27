945ce94804cd368faf4756837dbcf0dbb18e44ba
sanketardekar <ardekarsanket@gmail.com>
2021-11-27 11:22:39 +0530

    Implemented user authentication (signup, login, logout)

---
+++ lib/authentication/check_logged_in.dart
@@ -0,0 +1,17 @@
+import '../screens/home_screen.dart';
+import './login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class CheckLoggedIn extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (_auth.currentUser != null) {
+      return HomeScreen();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
